ffc374210e36b4434d4f9336ece3800b551671c9
lucasdantas2014 <lucas.dantasifpb@gmail.com>
2016-08-30 02:39:43 -0300

    1parteExercicioProva
    questões 11 a 15 prontas
    
    Teste.idea
    
    Tentar enviar o .idea
    
    PrimeiroCommint
    
    Questão11
    sem o . idea
    para colocar o repositorio local
    no remoto
    
    :lollipop: Added .gitattributes & .gitignore files
    
    :lollipop: Added .gitattributes & .gitignore files

---
+++ exercicioProva2Bim/Questao15-EStrutura-de-Repeticao.py
@@ -0,0 +1,20 @@
+n=eval(input("Digite até que valor será a série de Fibonacci:"))
+if(n >= 0):
+    a = True
+    x = 0
+    y = 1
+    while (a==True):
+        if(n==0):
+            print(x)
+        elif:
+            print(x)
+            print(y)
+            x = x + y
+            y = x + y
+        if (x > n or y > n):
+            a = False
+        else:
+             a = True
+else:
+    print("O valor não pode ser negativo")
+
